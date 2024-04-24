6aa711f6c3e410d662fd32b16de4e74903190cb3
martin.novak <rcdm@seznam.cz>
2024-04-24 15:11:58 +0200

    Python zakladni lekce po lekci 8

---
+++ Python_zakladni_lekce.py
@@ -0,0 +1,81 @@
+#print("papousek")
+#vstup = input("Napis neco:")
+#print("jsem papousek a opakuji:" + vstup + vstup + "!")
+
+#print("zdvojnásobovač")
+#vstup = int(input("zadej cislo:"))
+#vystup = vstup*2
+#print(vystup)
+
+#print("kalkulacka")
+#vstup1 = float(input("zadej prvni cislo:"))
+#vstup2 = float(input("zadej druhe cislo:"))
+#vystup1 = vstup1 + vstup2
+#vystup2 = vstup1 - vstup2
+#vystup3 = vstup1 * vstup2
+#vystup4 = vstup1 / vstup2
+#print("soucet:{0}\nrozdil:{1}\nsoucin: {2}\npodil: {3}\n".format(vystup1,vystup2,vystup3,vystup4))
+
+#print("kruh, obvod+obsah")
+#import math
+#polomer = float(input("zadej polomer:"))
+#print("obvod: {0}\nobsah: {1}".format(round(math.pi*2*polomer,2), round(math.pi* polomer* polomer,2)))
+
+#import decimal
+#print (decimal.Decimal("0.1") + decimal.Decimal("0.2"))
+
+#from fractions import Fraction
+#zlomek = Fraction(3, 4)  # reprezentuje zlomek 3/4
+
+#ESCAPE sekvence
+#\n novy radek
+#\t tabulator
+#\a zvonek
+#\\ specialni znak
+#\' uvozovky jednoduche
+#\" uvozovky dvojite
+
+#vstup = "Krokonosohroch"
+#print(vstup.startswith("krok"))
+#print(vstup.endswith("hroch"))
+#print("nos" in vstup)
+#print("roh" in vstup)
+
+#strip maze bile znaky pred a za retezcem
+
+#veta = "C# je nejlepší!"
+#veta = veta.replace("C#", "Python")
+#print(veta)
+
+#pokus = 15
+#match pokus:
+#   case 10:
+#       print("pokus je 10")
+#   case 20:
+#       print("pokus je 20")
+#   case _:
+#        print("zachyti ostatni hodnoty")
+
+#jmeno = input("zadej jmeno:")
+#jmeno = jmeno.strip()
+#if len(jmeno)>10:
+#    print("zadal jsi dlouhe jmeno")
+#elif len(jmeno)<3:
+#    print("zadal jsi kratke jmeno")
+#else:
+#    print("v pohode jmeno!")
+
+#print("vypocet kvadraticke rovnice")
+#a = float(input("zadej cislo a:"))
+#b = float(input("zadej cislo b:"))
+#c = float(input("zadej cislo c:"))
+#d=b**2 - 4*a*c
+#if d==0:
+#   vystup1 = (-b/2*a)
+#   print("rovnice ma jedno reseni!\nkoren rovnice:{0}".format(vystup1))
+#elif d>0:
+#    vystup1=(-b+d**1/2)/2*a
+#    vystup2=(-b-d**1/2)/2*a
+#   print("rovnice na 2 reseni!\nkoreny rovnice:{0},{1}".format(vystup1,vystup2))
+#:
+#   print("rovnice nema reseni!")
