b2a68dfc3728120d7685169088dad855cfee5008
martin.novak <rcdm@seznam.cz>
2024-04-25 12:30:58 +0200

    mala nasobilka ( for)

--- Python_zakladni_lekce.py
+++ Python_zakladni_lekce.py
@@ -65,16 +65,10 @@
 #else:
 #    print("v pohode jmeno!")
 
-#print("vypocet kvadraticke rovnice")
-#a = float(input("zadej cislo a:"))
-#b = float(input("zadej cislo b:"))
-#c = float(input("zadej cislo c:"))
-#d=b**2 - 4*a*c
-#if d==0:
-#   vystup1 = (-b/2*a)
-#   print("rovnice ma jedno reseni!\nkoren rovnice:{0}".format(vystup1))
-#elif d>0:
-#    vystup1=(-b+d**1/2)/2*a
-#    vystup2=(-b-d**1/2)/2*a
-#   print("rovnice na 2 reseni!\nkoreny rovnice:{0},{1}".format(vystup1,vystup2))
-#   print("rovnice nema reseni!")
+
+
+
+
+
+
+
